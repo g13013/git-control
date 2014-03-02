994c66d95e52adb0d18a485c8e6073ba9358421e
Aboubakr Gasmi <g13013@gmail.com>
2014-03-03 00:26:37 +0100

    configure grunt, travis and jshint

---
+++ test/config_parser_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gitConfig = require('../lib/utils/git-config-parser.js');
+
+exports['parse'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'config': function(test) {
+    var expected,
+        config = gitConfig.parse('./test/sample_config');
+    test.expect(1);
+    expected = {
+      core: {
+        repositoryformatversion: 0,
+        filemode: true,
+        bare: false
+      },
+      remote: {
+        origin: {
+          url: 'https://github.com/g13013/git-control.git',
+          fetch: '+refs/heads/*:refs/remotes/origin/*'
+        }
+      },
+      branch: {
+        master: {
+          remote: 'origin',
+          merge: 'refs/heads/master'
+        }
+      }
+    };
+    test.deepEqual(config, expected, 'should return an object of sample_config');
+    test.done();
+  },
+};
